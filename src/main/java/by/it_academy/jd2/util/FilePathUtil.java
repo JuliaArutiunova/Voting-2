6d7e3a81d9e46560ba4fd75e7c818ad125160837
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-09-28 16:04:23 +0300

    Добавлен FilePathUtil

--- src/main/java/by/it_academy/jd2/util/FilePathUtil.java
+++ src/main/java/by/it_academy/jd2/util/FilePathUtil.java
@@ -5,6 +5,9 @@ public final class FilePathUtil {
     public final static String MESSAGE_PAGE_PATH = "/template/message.jsp";
     public final static String ACCEPTED_PAGE_PATH = "/template/accepted.jsp";
     public final static String ERROR_PAGE_PATH = "/template/voteError.jsp";
+    public final static String RESULTS_JSP_PATH = "/template/votingResults.jsp";
+    public static final String ARTIST_JSP_PATH = "/template/artist.jsp";
+    public static final String GENRE_JSP_PATH = "/template/genre.jsp";
 
     private FilePathUtil() {
     }
