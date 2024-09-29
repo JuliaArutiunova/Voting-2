cabd3af2bf301592959ab6232e7716adeab1c9d5
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-09-29 12:55:55 +0300

    Добавлен VotingFormValidator в конструктор

--- src/main/java/by/it_academy/jd2/service/factory/VoteServiceFactory.java
+++ src/main/java/by/it_academy/jd2/service/factory/VoteServiceFactory.java
@@ -1,18 +1,24 @@
 package by.it_academy.jd2.service.factory;
 
 import by.it_academy.jd2.service.VoteService;
+import by.it_academy.jd2.service.api.IVoteService;
 import by.it_academy.jd2.storage.factory.StorageFactory;
-import by.it_academy.jd2.validation.VotingFormValidator;
+import by.it_academy.jd2.validation.factory.ValidatorFactory;
 
 public class VoteServiceFactory {
 
-    private static final VoteService instance= new VoteService(StorageFactory.getVotingStorage(),
-            new VotingFormValidator(ArtistServiceFactory.getInstance(),GenreServiceFactory.getInstance()));
+
+    private static final IVoteService instance = new VoteService(StorageFactory.getVotingStorage()
+    ,ValidatorFactory.getInstance());
 
     private VoteServiceFactory() {
     }
 
-    public static VoteService getInstance(){
+    public static IVoteService getInstance() {
         return instance;
     }
+
+
 }
+
+
