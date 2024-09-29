fe6fc7a401f4ceb9c56d474acf185bddf0c03124
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-09-29 13:02:15 +0300

    Исправлен конструктор в VoteServiceFactory

--- src/main/java/by/it_academy/jd2/service/factory/VoteServiceFactory.java
+++ src/main/java/by/it_academy/jd2/service/factory/VoteServiceFactory.java
@@ -2,13 +2,13 @@ package by.it_academy.jd2.service.factory;
 
 import by.it_academy.jd2.service.VoteService;
 import by.it_academy.jd2.service.api.IVoteService;
-import by.it_academy.jd2.storage.factory.StorageFactory;
+import by.it_academy.jd2.storage.db.factory.VotingStorageFactory;
 import by.it_academy.jd2.validation.factory.ValidatorFactory;
 
 public class VoteServiceFactory {
 
 
-    private static final IVoteService instance = new VoteService(StorageFactory.getVotingStorage()
+    private static final IVoteService instance = new VoteService(VotingStorageFactory.getInstance()
     ,ValidatorFactory.getInstance());
 
     private VoteServiceFactory() {
